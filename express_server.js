26c72c190dc69e1e92fc6f0436666562a6e9a9e7
faez <faezc@hotmail.com>
2021-09-30 06:47:10 +0000

    added the ability for users to only see their own shortened urls

--- express_server.js
+++ express_server.js
@@ -40,6 +40,18 @@ const users = {
   }
 };
 
+const urlsForUser = id => {
+  let matchingURLs = {};
+
+  for (const url in urlDatabase) {
+    if (urlDatabase[url]["userID"] === id) {
+      matchingURLs[url] = url;
+    }
+  }
+  console.log(matchingURLs);
+  return matchingURLs;
+};
+
 // function to search our users database by an email parameter (literally an email); it returns an object containing the user's data
 const findUserByEmail = (email) => {
   for (const userID in users) {
@@ -62,15 +74,23 @@ const generateRandomString = () => {
 
 // handles get requests for the main page and reroutes you to the /urls/ page
 app.get('/', (req, res) => {
+  const userID = req.cookies['user_id'];
+
+  if (!userID) {
+    res.redirect('/login');
+  }
+  
   res.redirect('/urls');
 });
 
 // handles get method for /login path and returns the login page
 app.get('/login', (req, res) => {
   const userID = req.cookies['user_id'];
+  
   const templateVars = {
     userObj: users[userID],
   };
+
   res.render('login_page', templateVars);
 });
 
@@ -147,6 +167,10 @@ app.get('/u/:shortURL', (req, res) => {
 app.get('/urls', (req, res) => {
   const userID = req.cookies['user_id'];
   
+  if (!userID) {
+    res.redirect('/login');
+  }
+  
   const userURLs = {};
 
   for (const shortURL in urlDatabase) {
@@ -158,6 +182,7 @@ app.get('/urls', (req, res) => {
   const templateVars = {
     userObj: users[userID],
     urls: userURLs,
+    userID: userID
   };
   res.render('urls_index', templateVars);
 });
@@ -180,12 +205,15 @@ app.post('/urls', (req, res) => {
 // presents the urls/new page containing a form to input a URL
 app.get('/urls/new', (req, res) => {
   const userID = req.cookies['user_id'];
+  
   if (!userID) {
     res.redirect('/login');
   }
+  
   const templateVars = {
     userObj: users[userID],
   };
+  
   res.render('urls_new', templateVars);
 });
 
@@ -206,7 +234,18 @@ app.post('/urls/:shortURL/edit', (req, res) => {
 // handles the route when a shortURL is provided and passes along an obj containing both the shortURLs and longURLs
 app.get('/urls/:shortURL', (req, res) => {
   const userID = req.cookies['user_id'];
+
+  if (!userID) {
+    res.redirect('/login');
+  }
+
   const shortURL = req.params.shortURL;
+
+  // this if statement might need to be rewritten (although it could be correct)
+  if (!urlsForUser(userID)[shortURL]) {
+    res.status(401).send("You must login to view this shortened URL")
+  }
+
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL]["longURL"],
